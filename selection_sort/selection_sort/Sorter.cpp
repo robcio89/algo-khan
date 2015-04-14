41eb13f863afe8fffe34206abcddfede18db06e4
robcio89 <robcio89@gmail.com>
2015-04-14 12:12:45 +0200

    working UT for index_of_minimum function

--- selection_sort/selection_sort/Sorter.cpp
+++ selection_sort/selection_sort/Sorter.cpp
@@ -14,11 +14,11 @@ int Sorter::index_of_minimum(std::vector<int> list, unsigned int start_index)
 	auto min_val = list[start_index];
 	auto min_index = start_index;
 
-	for (const auto& i : list)
+	for (auto i = start_index + 1; i < list.size(); ++i)
 	{
-		if (i < min_val)
+		if (list[i] < min_val)
 		{
-			min_val = i;
+			min_val = list[i];
 			min_index = i;
 		}
 	}
