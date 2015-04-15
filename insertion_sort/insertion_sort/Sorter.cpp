605bc77b5a4131d831eaa58aec56b970923b3f01
robcio89 <robcio89@gmail.com>
2015-04-15 11:08:08 +0200

    ut for insert function, revert compiler settings

--- insertion_sort/insertion_sort/Sorter.cpp
+++ insertion_sort/insertion_sort/Sorter.cpp
@@ -2,6 +2,12 @@
 
 void Sorter::insert(std::vector<int>& input, unsigned int right_index, int value)
 {
+	auto i = 0;
+	for (i = right_index; i >= 0 && input[i] > value; i--)
+	{
+		input[i + 1] = input[i];
+	}
+	input[i + 1] = value;
 }
 
 Sorter::Sorter()
--- insertion_sort/insertion_sort_test/SorterTest.cpp
+++ insertion_sort/insertion_sort_test/SorterTest.cpp
@@ -12,7 +12,44 @@ namespace insertion_sort_test
 		
 		TEST_METHOD(insert_test_1)
 		{
-			// TODO: Your test code here
+			Sorter s;
+			std::vector<int> input { 3, 5, 7, 11, 13, 2, 9, 6 };
+			std::vector<int> output { 2, 3, 5, 7, 11, 13, 9, 6 };
+
+			s.insert(input, 4, 2);
+
+			for (auto i = 0; i < input.size(); ++i)
+			{
+				Assert::AreEqual(input[i], output[i]);
+			}
+		}
+
+		TEST_METHOD(insert_test_2)
+		{
+			Sorter s;
+			std::vector<int> input{ 2, 3, 5, 7, 11, 13, 9, 6 };
+			std::vector<int> output{ 2, 3, 5, 7, 9, 11, 13, 6 };
+
+			s.insert(input, 5, 9);
+
+			for (auto i = 0; i < input.size(); ++i)
+			{
+				Assert::AreEqual(input[i], output[i]);
+			}
+		}
+
+		TEST_METHOD(insert_test_3)
+		{
+			Sorter s;
+			std::vector<int> input{ 2, 3, 5, 7, 9, 11, 13, 6 };
+			std::vector<int> output{ 2, 3, 5, 6, 7, 9, 11, 13 };
+
+			s.insert(input, 6, 6);
+
+			for (auto i = 0; i < input.size(); ++i)
+			{
+				Assert::AreEqual(input[i], output[i]);
+			}
 		}
 
 	};
