55e1ad8b6a62bed54168406f2c5fe494f34123c9
robcio89 <robcio89@gmail.com>
2015-05-08 15:26:07 +0200

    added swap function

--- quick_sort/quick_sort/QuickSort.cpp
+++ quick_sort/quick_sort/QuickSort.cpp
@@ -1,5 +1,16 @@
 #include "QuickSort.h"
 
+int QuickSort::partition(std::vector<int>& input, unsigned int p, unsigned int r)
+{
+	return 1;
+}
+
+void QuickSort::swap(std::vector<int>& input, unsigned int first_index, unsigned int second_index)
+{
+	int temp = input[first_index];
+	input[first_index] = input[second_index];
+	input[second_index] = temp;
+}
 
 QuickSort::QuickSort()
 {
--- quick_sort/quick_sort/QuickSort.h
+++ quick_sort/quick_sort/QuickSort.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <vector>
+
 class QuickSort
 {
 public:
+	int partition(std::vector<int>& input, unsigned int p, unsigned int r);
 	QuickSort();
 	~QuickSort();
+
+private:
+	void swap(std::vector<int>& input, unsigned int first_index, unsigned int second_index);
 };
 
