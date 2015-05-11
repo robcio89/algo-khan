ae3bb7de06ae38121c93d0210469c3118176ec55
robcio89 <robcio89@gmail.com>
2015-05-11 10:42:07 +0200

    small changes in ut

--- quick_sort/quick_sort_test/QuickSortTest.cpp
+++ quick_sort/quick_sort_test/QuickSortTest.cpp
@@ -17,9 +17,11 @@ namespace quick_sort_test
 			std::vector<int> input = { 9, 7, 5, 11, 12, 2, 14, 3, 10, 6 };
 			std::vector<int> output = { 2, 3, 5, 6, 7, 9, 10, 11, 12, 14 };
 
-			qc.sort(input, 0, input.size() - 1);
+			auto input_size = input.size();
 
-			for (int i = 0; i < input.size(); ++i)
+			qc.sort(input, 0, input_size - 1);
+
+			for (int i = 0; i < input_size; ++i)
 			{
 				Assert::AreEqual(input[i], output[i]);
 			}
@@ -32,9 +34,11 @@ namespace quick_sort_test
 			std::vector<int> input = { 9, 7, 5, 11, 12, 2, 14, 3, 10, -3, 6 };
 			std::vector<int> output = { -3, 2, 3, 5, 6, 7, 9, 10, 11, 12, 14 };
 
-			qc.sort(input, 0, input.size() - 1);
+			auto input_size = input.size();
 
-			for (int i = 0; i < input.size(); ++i)
+			qc.sort(input, 0, input_size - 1);
+
+			for (int i = 0; i < input_size; ++i)
 			{
 				Assert::AreEqual(input[i], output[i]);
 			}
